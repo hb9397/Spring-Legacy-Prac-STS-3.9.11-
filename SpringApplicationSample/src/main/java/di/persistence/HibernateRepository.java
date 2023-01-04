7c8415f6da97bd60176db18d6db4832f08c7c5fd
hb9397@naver.com <hb9397@naver.com>
2023-01-04 18:00:36 +0900

    SpringPrac 20230104 -1
    Spring Legacy STS-Simple Spring Web Maven
    
    DB 연동, DB bean 설정 Transaction bean 설정수정

--- SpringApplicationSample/src/main/java/di/persistence/HibernateRepository.java
+++ SpringApplicationSample/src/main/java/di/persistence/HibernateRepository.java
@@ -1,22 +1,21 @@
-package di.persistence;
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
-import domain.Good2;
-
-@Repository
-public class HibernateRepository {
-	
-	@Autowired
-	private SessionFactory sessionFactory;
-	
-	public void insertGood(Good2 good) {
-		Session session = sessionFactory.getCurrentSession();
-		
-		// 데이터 추가
-		session.save(good);
-	}
-}
+/*
+ * package di.persistence;
+ * 
+ * import org.hibernate.Session; import org.hibernate.SessionFactory; import
+ * org.springframework.beans.factory.annotation.Autowired; import
+ * org.springframework.stereotype.Repository;
+ * 
+ * import domain.Good2;
+ * 
+ * @Repository public class HibernateRepository {
+ * 
+ * @Autowired private SessionFactory sessionFactory;
+ * 
+ * public void insertGood(Good2 good) { Session session =
+ * sessionFactory.getCurrentSession();
+ * 
+ * // 데이터 추가 session.save(good); }
+ * 
+ * 
+ * }
+ */
--- SpringApplicationSample/src/main/java/di/persistence/TransactionRepository.java
+++ SpringApplicationSample/src/main/java/di/persistence/TransactionRepository.java
@@ -3,24 +3,31 @@ package di.persistence;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class TransactionRepository {
+	
 	@Autowired
 	private SimpleJdbcInsert template;
 	
+	//중간에 예외가 발생하면 모든 작업이 Rollback
+	//대부분의 경우 트랜잭션 적용은 Service 메서드에 합니다.
+	@Transactional
 	public void insert() {
 		template.withTableName("goods");
 		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("code", 100);
-		map.put("name", "쌀");
-		map.put("manufacture", "경기도 이천");
-		map.put("price", 10000);
+		map.put("code", 300);
+		map.put("name", "우럭");
+		map.put("manufacture", "남해");
+		map.put("price", 20000);
 		
-		template.execute(map); // 정상작동
-		map.put("code", 100); // 기본키 중복으로 인한 에러 발생
+		template.execute(map);
+		template.execute(map); //기본키 중복으로 예외가 발생
 	}
+
 }
