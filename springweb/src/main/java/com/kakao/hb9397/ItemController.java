07743d0bb5a8124ba08d8ad06ded82048f58f7fc
hb9397@naver.com <hb9397@naver.com>
2023-01-06 14:32:29 +0900

    SpringPrac 20230106 -3
    Spring Legacy STS - Spring MVC Project
    
    Spring MVC Project Maven 기반의 Tomcat Server Web
    
    RDBMS를 연동하는 Application
    
    예외발생

--- springweb/src/main/java/com/kakao/hb9397/ItemController.java
+++ springweb/src/main/java/com/kakao/hb9397/ItemController.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -93,6 +94,15 @@ public class ItemController {
 		model.addAttribute("result", result);
 		return "result";
 	}
+	
+	// 예외가 발생했을 때 처리하기 위한 메서드
+	@ExceptionHandler(Exception.class)
+	public String handleException(Model model, Exception e) {
+		model.addAttribute("result", e.getLocalizedMessage());
+		
+		return "error/exception";
+	}
+	
 }
 
 
--- springweb/src/main/webapp/WEB-INF/views/result.jsp
+++ springweb/src/main/webapp/WEB-INF/views/result.jsp
@@ -4,7 +4,7 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>result</title>
 </head>
 <body>
 	결과:${result}
